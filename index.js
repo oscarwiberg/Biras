8751e24c47b8745ac46604bd6359a8890c1f9f60
oscarwiberg <oscar.wiberg@live.se>
2020-09-08 15:47:14 +0200

    initial

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
