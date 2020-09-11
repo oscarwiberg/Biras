a6995bbee410896612a03e87e6a0096c633ee035
oscarwiberg <oscar.wiberg@live.se>
2020-09-11 13:29:33 +0200

    404 component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/Header';
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import Contact from './pages/Contact';
 import News from './pages/News';
 import Products from './pages/Products';
+import NoMatch from './pages/NoMatch';
 
 function App() {
   const [tests, setTests] = useState([]);
@@ -23,10 +24,13 @@ function App() {
     <div className="App">
       <Router>
         <Header />
-        <Route path="/" exact component={Home} />
-        <Route path="/products" component={Products} />
-        <Route path="/news" component={News} />
-        <Route path="/contact" component={Contact} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/products" component={Products} />
+          <Route path="/news" component={News} />
+          <Route path="/contact" component={Contact} />
+          <Route component={NoMatch} />
+        </Switch>
       </Router>
     </div>
   );
